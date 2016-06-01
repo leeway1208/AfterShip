346d8730c6db45e25c7e2e860e388cfbb14e9eb0
leeway1208 <leeway1208@vip.qq.com>
2016-06-01 09:11:35 +0800

    Initial Commit

---
+++ AfterShip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AfterShip
+//
+//  Created by liwei wang on 1/6/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AfterShip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AfterShip
+//
+//  Created by liwei wang on 1/6/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
